71073a1445cef0fdea919c0fbb6ba6ee34835a00
Bamzzzz <muyiwaayobami@yahoo.com>
2023-09-22 09:32:25 -0700

    write digit

--- write_digit.c
+++ write_digit.c
@@ -1,105 +1,88 @@
 #include <unistd.h>
-#include "main.h"
+#include <stdlib.h>
 /**
- * write_digit - write all characters in digits
- * @num: the numbers of all the characters in string
- * Return: all written digits
+ * write_digit - write digit for each string in character
+ * @str: calculate string of lenght
+ * Return: integer
  */
+int _str_len(char *str);
 void write_digit(int num)
 {
-	int count = count_digit(num, 0);
-	char num_string[20];
+	char num_string[80];
 	char *ptr = insert_digit(num, num_string, 0);
-	char ch;
 
-	if (ptr[0] == '-')
-	{
-		write(1, "-", 1);
-		count--;
-		for ( ; count >= 1; count--)
-		{
-			ch = ptr[count];
-			write(1, &ch, 1);
-		}
-	}
-	else
-	{
-		count--;
-		for ( ; count >= 0; count--)
-		{
-			ch = ptr[count];
-			write(1, &ch, 1);
-		}
-	}
+	write(1, ptr, _str_len(ptr));
 }
 /**
- * insert_digit - insert all digits in the string of characters
- * @num: the parameter of the characters
- * @str: string of the characters
- * @i: index at which to insert digits
- * Return: character
+ * insert_digit - insert digits in character
+ * @num: number of digit in character
+ * @str: calculate strings in character
+ * @i: iterator
+ * Return: return all character
  */
 char *insert_digit(int num, char *str, int i)
 {
-	int remainder;
+
+	int remainder, j, temp;
 
 	if (num < 0)
-	{
-		str[i] = '-';
-		i = 1;
-		num = num * (-1);
-	}
-	if (num == 0)
-	{
-		str[i] = '0';
-		str[i + 1] = '\0';
-		return (str);
-	}
+	str[i] = '-';
+	num = num / (-1);
+	i = 1;
+}
+if (num == 0)
+{
+	str[i] = '0';
+	str[i + 1] = '\0';
+	return (str);
+}
+temp = count_digit(num);
+str[temp] = '\0';
+temp--;
+for (j = temp; j >= 1; j--)
+{
 	remainder = num % 10;
-	str[i] = remainder + '0';
-	if (num >= 10)
+	if (remainder < 0)
 	{
-		return (insert_digit(num / 10, str, i + 1));
+		remainder = remainder * (-1);
 	}
-	else
-	{
-		str[i + 1] = '\0';
-		return (str);
-	}
+	str[j] = remainder + '0';
+	num /= 10;
+}
+return (str);
 }
 /**
- * count_digit - the numbers of digits to be counted in the character
- * @num: the parameter of the integer
- * @count: the number of occurence of an element in a given range
- * Return: the numbers of  integer
+ * count_digit - count digit in the character
+ * @num: number of strings in integer
+ * Return: all integer
  */
-int count_digit(int num, int count)
+int count_digit(int num)
 {
-	int remainder, res;
+	int len = 0;
 
 	if (num < 0)
-	{
-		count = 1;
-		num = num * (-1);
-	}
-	if (num < 10)
-	{
-		count++;
-		return (count);
-	}
-	if (num == 10)
-	{
-		count += 2;
-		return (count);
-	}
-	if (num < 100 && num > 10)
-	{
-		count += 2;
-		return (count);
-	}
-	remainder = num % 10;
-	res = num - remainder;
-	res = res / 10;
-	count++;
-	return (count_digit(res, count));
+{
+	len = 1;
+	num = -num;
+}
+do {
+	len++;
+	num = num / 10;
+} while (num != 0);
+return (len);
+}
+/**
+ * _str_len - describe all the strings in the length of the character
+ * @str: calculate all the string in character
+ * Return: integer
+ */
+int _str_len(char *str)
+{
+	int i;
+	int size = 0;
+for (i = 0; str[i] != '\0'; i++)
+{
+size++;
+}
+return (size);
 }
